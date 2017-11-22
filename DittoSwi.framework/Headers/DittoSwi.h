651dd8e735f934f4bfa99328bcf65e6dc5b093ab
ikeda <ikeda@itsherpa.com>
2017-11-22 12:53:35 +0900

    add framework

---
+++ DittoSwi.framework/Headers/DittoSwi.h
@@ -0,0 +1,24 @@
+//
+//  DittoSwi.h
+//  DittoSwi
+//
+//  Created by ikeda on 2017/11/10.
+//  Copyright © 2017年 SampCom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DittoSwi.
+FOUNDATION_EXPORT double DittoSwiVersionNumber;
+
+//! Project version string for DittoSwi.
+FOUNDATION_EXPORT const unsigned char DittoSwiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DittoSwi/PublicHeader.h>
+
+//#import <DittoSwi/DittoSwi.h>
+//#import <DittoSwi/DittoSwi.swift>
+
+
+//#import "DittoSwi.swift";
+
---
+++ DittoSwi.framework/Headers/DittoSwi2.swift
@@ -0,0 +1,19 @@
+//
+//  DittoSwi2.swift
+//  DittoSwi
+//
+//  Created by ikeda on 2017/11/22.
+//  Copyright © 2017年 SampCom. All rights reserved.
+//
+
+import Foundation
+
+public class DittoSwi2:NSObject {
+    
+    /**
+     Frameworkで提供している機能
+     */
+    public func inputHadou() -> String {
+        return "Hadou拳"
+    }
+}
